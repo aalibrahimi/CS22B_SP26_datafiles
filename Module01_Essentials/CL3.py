90d20b1f3587eebad592d267c6081118f5f786b4
aalibrahimi <aalibrahimi0@gmail.com>
2026-02-12 09:58:47 -0800

    Completed CL3 list tuples, dicts homework

--- Module01_Essentials/CL3.py
+++ Module01_Essentials/CL3.py
@@ -6,13 +6,21 @@
 ##### CL3.1: List comprehension. Given:
 NL = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 ## Write a list comprehension to collect the diagonal items from matrix NL
-# ** your code **
+# Enumerate: [(0, [1, 2, 3]), (1, [4, 5, 6]), (2, [7, 8, 9])]
+diag = [item[idx] for (idx, item) in enumerate(NL)]
+print(diag)
 
 ##### CL3.2: Using my_list, create a list of tuples where tuple is (n, n**3) where n is each number in the list.
 my_list = [1, 2, 3, 4, 5]
-# ** your code **
+new_list = [(n, n**3) for n in my_list]
+print(new_list)
+
 
 
 ##### CL3.3: Multiple all the values of the items in a dictionary 
 num_dict = {'num1':100, 'num2':346, 'num3':-24}
-# ** your code **
+res = 1
+for n in num_dict.values():
+    res *= n
+
+print(res)
